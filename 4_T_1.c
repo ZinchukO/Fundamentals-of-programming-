4b846a7e8a964faf2d6474a47c0954099ef2a1a1
ZinchukO <Ostap.Zinchuk@lnu.edu.ua>
2024-10-03 10:31:19 +0300

    Update 4_T_1.c

--- 4_T_1.c
+++ 4_T_1.c
@@ -1,76 +1,30 @@
 #include <stdio.h>
 
+int main(void) {
 
-int main() {
-    int a = printf("Введіть перше число:\n");
-    scanf("%d", &a);
-    int b = printf("Введіть друге число:\n");
-    scanf("%d", &b);
-    int result;
+  int number = printf("Введіть число 1: ");
+  scanf("%d", &number);
+  void *zxc = &number;
 
-    printf("Арифметичні оператори:\n");
-    printf("a = %d, b = %d\n", a, b);
+  printf("десяткове = %d\n", number);
+  printf("двійкове = %b\n", number);
+  printf("шістнадцяткове = %x\n", number);
+  printf("Biciмкове = %o\n", number);
 
-    // Додавання
-    result = a + b;
-    printf("a + b = %d\n", result);
+  float floatn = printf("Введіть число 2: ");
+  scanf("%f", &floatn);
+  
+  printf("плаваюча = %f\n", floatn);
+  printf("експонційна = %e\n", floatn);
+  printf("гнучка = %g\n", floatn);
+  
+  char letter = printf("Введіть букву: ");
+  scanf("%c", &letter);
+  char strin[] = "Hello World";
 
-    // Віднімання
-    result = a - b;
-    printf("a - b = %d\n", result);
-
-    // Множення
-    result = a * b;
-    printf("a * b = %d\n", result);
-
-    // Ділення
-    result = a / b;
-    printf("a / b = %d\n", result);
-
-    // Остача від ділення
-    result = a % b;
-    printf("a %% b = %d\n", result);
-
-    printf("\nЛогічні оператори:\n");
-
-    // Логічне AND
-    printf("(a && b) = %d\n", a && b);
-
-    // Логічне OR
-    printf("(a || b) = %d\n", a || b);
-
-    // Логічне NOT
-    printf("!a = %d\n", !a);
-
-    printf("\nПобітові оператори:\n");
-    printf("a в двійковому вигляді: %b\n", a);
-    printf("b в двійковому вигляді: %b\n", b);
-
-
-    // Побітове AND
-    result = a & b;
-    printf("a & b = %b\n", result);
-
-    // Побітове OR
-    result = a | b;
-    printf("a | b = %b\n", result);
-
-    // Побітове XOR
-    result = a ^ b;
-    printf("a ^ b = %b\n", result);
-
-    // Побітове доповнення
-    result = ~a;
-    printf("~a = %b\n", result);
-
-    // Побітовий зсув вліво
-    result = a << 1;
-    printf("a << 1 = %b\n", result);
-    
-
-    // Побітовий зсув вправо
-    result = a >> 1;
-    printf("a >> 1 = %b\n", result);
-
-    return 0;
+  printf("символ = %c\n", letter);
+  printf("стрічка = %s\n", strin);
+  printf("вказiвник = %p\n", zxc);
+  
+  return 0;
 }
